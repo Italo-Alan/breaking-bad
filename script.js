ec888293dc665b240b1a91ab8d4fa98f82aa75a6
Italo Alan <italoalan123@gmail.com>
2022-09-13 20:36:20 -0300

    Primeiro commit do Site

--- script.js
+++ script.js
@@ -2,21 +2,20 @@ const home = document.querySelector('.home');
 home.addEventListener('click', () =>{
     window.scrollTo(0,0)
 });
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+}
+
+
 let response;
-let bad;
-let html = "";
 const characters = document.querySelector('.characters')
 const url = "https://www.breakingbadapi.com/api/characters"
-
-//     let results = await res.json();
-
-//     results.map(result =>{
-        // const htmlString = `<img src="${result.img}" class="img">
-        // <div class="info-display">
-        // `
-
-//     })
-// }
 
 fetch(url)
     .then(function (response){
@@ -24,27 +23,24 @@ fetch(url)
     })
     .then(function (response){
         console.log(response)
+        let html ="";
         for(let i = 0; i <= response.length; i++){
-            var img = document.createElement("IMG");
-            img.src = response[i].img;
-            img.style.width = "200px";
-            document.querySelector('.imageCharacter').appendChild(img);
-            document.querySelector('.infoCharacter').textContent += response[i].name;
+            html = `
+            <div class="p-card">
+                <div class="p-card_image">
+                <img src="${response[i].img}" class="p-card_content img2">
+                </div>
+                <div class="p-card_content">
+                    <span class="character-name" style="width: 100%">Nome: ${response[i].name}</span>
+                    <span class="character-portrayed" style="width: 100%">Ator/Atriz: ${response[i].portrayed}</span>
+                    <span class="character-status" style="width: 85%">Status: ${response[i].status}</span>
+                </div>
+                </div>
+            `
+            document.querySelector('.p-cards').innerHTML += html;
         }
 
-            /*
-                response[i].name
-                response[i].occupation
-                response[i].portrayed
-                response[i].status
-            */
-
-        //Funciona bem
-        // console.log(response)
-        // for(let c = 0; c <= response.length; c++){
-        //     console.log(response[c].name, response[c].birthday, response[c].status)
-        // }
     })
-    .catch (function(err){
-        console.error(`Failed retrieving information ${err}`);
+    .catch (function(error){
+        console.error(`Failed retrieving information ${error}`);
     });
