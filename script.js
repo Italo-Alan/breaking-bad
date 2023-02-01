44d176511d216d74fe787bdf5be9a904f7c9d78e
Italo-Alan <italoalan123@gmail.com>
2023-02-01 12:18:26 -0300

    :rocket: Aparentemente a API deixou de funcionar, implementei o conteúdo dos parágrafos por meio de JS, não mais por HTML

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { text } from './content.js';
+
 /*SCROLL HOME*/
 const home = document.querySelector('.home');
 home.addEventListener('click', () =>{
@@ -57,7 +59,7 @@ const characters = document.querySelector('.characters');
 const url = "https://www.breakingbadapi.com/api/characters";
 
 fetch(url)
-    .then(function (response){
+.then(function (response){
         return response.json();
     })
     .then(function (bad){
@@ -65,15 +67,15 @@ fetch(url)
         for(let i = 0; i <= bad.length; i++){
             html = `
             <div class="p-card">
-                <div class="p-card_image">
-                <img src="${bad[i].img}" class="p-card_content img2">
-                </div>
-                <div class="p-card_content">
-                    <span class="character-name" style="width: 100%">Nome: ${bad[i].name}</span>
-                    <span class="character-portrayed" style="width: 100%">Ator/Atriz: ${bad[i].portrayed}</span>
-                    <span class="character-status" style="width: 85%">Status: ${bad[i].status}</span>
-                </div>
-                </div>
+            <div class="p-card_image">
+            <img src="${bad[i].img}" class="p-card_content img2" alt="Imagem da(o) personagem ${bad[i].name}, vivida(o) por ${bad[i].portrayed}">
+            </div>
+            <div class="p-card_content">
+            <span class="character-name" style="width: 100%">Nome: ${bad[i].name}</span>
+            <span class="character-portrayed" style="width: 100%">Ator/Atriz: ${bad[i].portrayed}</span>
+            <span class="character-status" style="width: 85%">Status: ${bad[i].status}</span>
+            </div>
+            </div>
             `;
             document.querySelector('.p-cards').innerHTML += html;
         }
@@ -81,3 +83,41 @@ fetch(url)
     .catch (function(error){
         console.error(`Failed retrieving information ${error}`);
     });
+
+    
+    let firstSeason, secondSeason, thirdSeason, fourthSeason, fifthSeason = "";
+
+    for(let i = 0; i < text[0].length;i++){
+        firstSeason = `
+            <p>${text[0][i]}</p>
+        `
+        document.querySelector('.firstSeason').innerHTML += firstSeason;
+    };
+
+    for(let i = 0; i < text[1].length;i++){
+        secondSeason = `
+            <p>${text[1][i]}</p>
+        `
+        document.querySelector('.secondSeason').innerHTML += secondSeason;
+    };
+
+    for(let i = 0; i < text[2].length;i++){
+        thirdSeason = `
+            <p>${text[2][i]}</p>
+        `
+        document.querySelector('.thirdSeason').innerHTML += thirdSeason;
+    };
+
+    for(let i = 0; i < text[3].length;i++){
+        fourthSeason = `
+            <p>${text[3][i]}</p>
+        `
+        document.querySelector('.fourthSeason').innerHTML += fourthSeason;
+    };
+
+    for(let i = 0; i < text[4].length;i++){
+        fifthSeason = `
+            <p>${text[4][i]}</p>
+        `
+        document.querySelector('.fifthSeason').innerHTML += fifthSeason;
+    };
